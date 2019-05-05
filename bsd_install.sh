a62b103050d93d8179ba93ff15c8b9e4bb5a5569
natrix-fork <cqrdev@gmail.com>
2019-05-05 17:45:51 +0300

    Add files via upload

---
+++ bsd_install.sh
@@ -0,0 +1,20 @@
+chmod +x bsdAUTOPWN
+chmod +x bsdAUTOPWN.x64
+chmod +x bsdAUTOPWN.dfly.SVR4
+chmod +x exploits/0x82-apache-mod_jk.x
+chmod +x exploits/Femitter-Ftp_Service_Server_v1.x
+chmod +x exploits/Telnet-Ftp_Service_Server_v1.x
+chmod +x exploits/0x82-apache-mod_jk.x.x64
+chmod +x exploits/Femitter-Ftp_Service_Server_v1.x.x64
+chmod +x exploits/mod_shellcode.x.x64
+chmod +x exploits/telnetd-encrypt_keyid.x64
+chmod +x exploits/telnetd-encrypt_keyid-multi.x64
+chmod +x exploits/Telnet-Ftp_Service_Server_v1.x.x64
+chmod +x exploits/0x82-apache-mod_jk.SVR4
+chmod +x exploits/Femitter_FTP_Server_1.x Multiple_Vulnerabilities_(post auth).dfly.SVR4
+chmod +x exploits/Femitter-Ftp_Service_Server_v1.dfly.SVR4
+chmod +x exploits/mod_shellcode.dfly.SVR4
+chmod +x exploits/telnetd-encrypt_keyid.dfly.SVR4
+chmod +x exploits/telnetd-encrypt_keyid-multi.dfly.SVR4
+chmod +x exploits/Telnet-Ftp_Service_Server_v1.dfly.SVR4
+
